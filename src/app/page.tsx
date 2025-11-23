e1124910b384a1af4eab87b2120e5ee38ad7bac9
Mirko <mirkoat21@gmail.com>
2025-11-23 15:34:31 +0100

    fix: correct flex wrapping for desktop icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,9 @@
 import React from "react";
 
-const DesktopIcon = ({ color }: { color: string }) => (
-  <div
-    className={`w-[50px] h-[50px] mx-2 my-2`}
-    style={{ backgroundColor: color }}
-  />
-);
-
 const Page = () => {
   return (
     <div className="relative h-[calc(100vh-48px)] w-full">
-      <div className="flex flex-col flex-wrap h-full w-full">
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-        <DesktopIcon color="#FFA500" />
-      </div>
-
-      <div className="absolute bottom-0 right-0 w-[50px] h-[50px] mx-2 my-2 bg-[#ff0000]" />
+      <div className="flex flex-col flex-wrap content-start h-full w-full"></div>
     </div>
   );
 };
---
+++ src/components/essential/desktopicon.tsx
@@ -0,0 +1,8 @@
+const DesktopIcon = ({ color }: { color: string }) => (
+  <div
+    className={`w-[50px] h-[50px] mx-2 my-2`}
+    style={{ backgroundColor: color }}
+  />
+);
+
+export default DesktopIcon;
